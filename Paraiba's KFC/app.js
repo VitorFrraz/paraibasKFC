bde336677fae3501a0efe88f782f8d5ef0d2a00c
VitorFrraz <vitor_ferrazsantos@hotmail.com>
2024-05-29 08:20:08 -0300

    Add files via upload

---
+++ Paraiba's KFC/app.js
@@ -0,0 +1,58 @@
+console.log('sucessfully connected app.js')
+
+const bar = document.querySelector('#bar');
+const cross = document.querySelector('#cross');
+const nav = document.querySelector('.nav');
+
+bar.addEventListener('click', ()=> {
+    bar.style.display = 'none';
+    cross.style.display = 'block';
+    nav.style.left = '20%';
+})
+
+cross.addEventListener('click', ()=>{
+    cross.style.display = 'none';
+    bar.style.display ='block';
+    nav.style.left = '100%';
+})
+
+
+const arrow = document.querySelectorAll('.arrow');
+
+
+arrow.forEach((e) => {
+
+    
+e.addEventListener('click', () => {
+    
+    var footerlist = e.parentElement.nextElementSibling;
+    
+    if(e.classList.contains('temp')){
+      e.classList.remove('temp');
+      footerlist.style.display = 'none';
+      e.style.transform = 'rotate(0deg)';
+    }
+    else{
+        e.classList.add('temp'); 
+        footerlist.style.display = 'block';
+        e.style.transform = 'rotate(180deg)';
+    }
+
+
+});
+    
+})
+
+
+
+const menuNav = document.querySelector('.menu-nav')
+
+window.addEventListener('scroll', ()=> {
+
+    if(window.pageXOffset > menuNav.offsetTop){
+        menuNav.classList.add('sticky');
+    }
+    else{
+        menuNav.classList.remove('sticky');
+    }
+})
